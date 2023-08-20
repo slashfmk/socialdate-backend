b1b16d8dea5188b8dbb21fc5e9d1ab14e585ac30
Yannick fumukani <slashcs7@hotmail.com>
2023-08-19 23:40:13 -0500

    Init socialdate backend

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using socialbackend.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>((options) =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.UseCors(builder =>
+builder
+.AllowAnyHeader()
+.AllowAnyMethod()
+.WithOrigins("http://localhost:4200")
+);
+
+app.Run();
